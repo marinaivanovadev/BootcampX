d3c97a81d7d6da6ff1b062853101db27bf28e04a
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-08-03 13:38:18 -0700

    SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -12,13 +12,15 @@ pool.connect();
 const cohortName = process.argv[2];
 const maximumResult = process.argv[3];
 
-pool
-  .query(`SELECT students.id, students.name, cohorts.name as cohort_name
-  FROM students
-  JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name LIKE $1
-  LIMIT $2;
-  `, [`%${cohortName}%`, maximumResult])
+const queryString = `SELECT students.id, students.name, cohorts.name as cohort_name
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const values =  [`%${cohortName}%`, maximumResult];
+
+pool.query(queryString, values)
   .then(result => {
     result.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
